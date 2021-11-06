54c18452dad3ab53842ef85eb5c548fc849766be
Chad Johnson <chad.d.johnson@gmail.com>
2021-11-06 11:05:50 -0400

    Created skeletons for artist and profile pages.

---
+++ applications/directory/src/pages/artists/[slug].js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import { Layout } from '../../components';
+
+const ArtistPage = () => (
+  <>
+    <Head>
+      <title>Artist Name Here</title>
+    </Head>
+    <Layout>Artist details here</Layout>
+  </>
+);
+
+export default ArtistPage;
---
+++ applications/directory/src/pages/artists/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import { Layout } from '../../components';
+
+const ArtistsPage = () => (
+  <>
+    <Head>
+      <title>Artists</title>
+    </Head>
+    <Layout>Artist list here</Layout>
+  </>
+);
+
+export default ArtistsPage;
