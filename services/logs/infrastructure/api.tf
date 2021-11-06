34a246c3be8cd5545a89e0747a840ee310887c9e
Chad Johnson <chad.d.johnson@gmail.com>
2021-11-06 15:41:53 -0400

    Updated Logs service infrastructure.

--- services/logs/infrastructure/api.tf
+++ services/logs/infrastructure/api.tf
@@ -1,12 +1,11 @@
 locals {
-  mongodb_hosts = "${join(":27017,", data.terraform_remote_state.artistdirectory_infrastructure.outputs.services_domain_names)}:27017"
-  domain        = "logs-api.${data.aws_region.current.name}.${data.terraform_remote_state.artistdirectory_infrastructure.outputs.domain}"
+  domain = "logs-api.${data.terraform_remote_state.artistdirectory_infrastructure.outputs.domain}"
 }
 
 resource "aws_ssm_parameter" "mongodb_logs_database_url" {
   name      = "/artistdirectory/${terraform.workspace}/database/mongodb-logs/url"
   type      = "SecureString"
-  value     = "mongodb://app:${var.mongodb_app_password}@${local.mongodb_hosts}/artistdirectory-logs?replicaSet=rs0&readPreference=secondaryPreferred&w=1&wtimeoutMS=5000&ssl=true"
+  value     = "mongodb+srv://admin:${var.mongodb_app_password}@rs0.eceh0.mongodb.net/artistdirectory-directory?replicaSet=rs0&retryWrites=true&w=majority&wtimeoutMS=5000"
   overwrite = true
   provider  = aws.region
 }
