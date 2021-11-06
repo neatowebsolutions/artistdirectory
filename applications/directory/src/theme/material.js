55c42ce8107e5600ba0d0528cb6331d331fd3b87
Chad Johnson <chad.d.johnson@gmail.com>
2021-11-06 13:41:52 -0400

    Added Material UI style overrides.

--- applications/directory/src/theme/material.js
+++ applications/directory/src/theme/material.js
@@ -7,8 +7,30 @@ const theme = responsiveFontSizes(
         main: '#be2926'
       }
     },
-    overrides: {
-      //
+    breakpoints: {
+      values: {
+        mobile: 0,
+        tablet: 768,
+        laptop: 1024,
+        desktop: 1440
+      }
+    },
+    components: {
+      MuiCard: {
+        defaultProps: {
+          elevation: 6
+        }
+      },
+      MuiPaper: {
+        styleOverrides: {
+          root: {
+            paddingTop: 8 * 3,
+            paddingBottom: 8 * 3,
+            paddingLeft: 8 * 3,
+            paddingRight: 8 * 3
+          }
+        }
+      }
     }
   })
 );
