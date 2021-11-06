b200eb86c475c578a0066b6a1b670679ae027346
Todd VanHouten <todd.vanhouten@fusionary.com>
2021-11-06 12:51:36 -0400

    Add base directory api endpoints

---
+++ services/directory-api/src/routes/createArtist.js
@@ -0,0 +1,47 @@
+const { StatusCodes, ReasonPhrases } = require('http-status-codes');
+const logger = require('@artistdirectory/logger');
+const mongodbClient = require('../models/mongodbClient');
+const models = require('../models');
+
+const handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  await mongodbClient.connect();
+
+  if (event.source === 'serverless-plugin-warmup') {
+    await new Promise((resolve) => setTimeout(resolve, 25));
+    return 'Lambda is warm!';
+  }
+
+  try {
+    const Artist = await models.get('Artist');
+    const data = JSON.parse(event.body);
+    const artist = new Artist(data);
+
+    try {
+      await artist.validate();
+    } catch (error) {
+      return {
+        statusCode: StatusCodes.BAD_REQUEST,
+        body: ReasonPhrases.BAD_REQUEST
+      };
+    }
+
+    await artist.save();
+    await logger.info(`Artist created (${artist.toString()})`, { data });
+
+    return {
+      statusCode: StatusCodes.CREATED,
+      body: JSON.stringify(artist)
+    };
+  } catch (error) {
+    await logger.error(`Error creating artist`, error, { event });
+
+    return {
+      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      body: error.message || ReasonPhrases.INTERNAL_SERVER_ERROR
+    };
+  }
+};
+
+module.exports.handler = handler;
