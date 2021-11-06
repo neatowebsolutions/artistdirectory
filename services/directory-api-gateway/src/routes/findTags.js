cebca4d840d7a958daee4667e35c46377a40dd40
Todd VanHouten <todd.vanhouten@fusionary.com>
2021-11-06 13:06:55 -0400

    Add routes to gateway for directory api endpoints

---
+++ services/directory-api-gateway/src/routes/findTags.js
@@ -0,0 +1,58 @@
+const middy = require('@middy/core');
+const cors = require('@middy/http-cors');
+const {
+  StatusCodes,
+  ReasonPhrases,
+  getReasonPhrase
+} = require('http-status-codes');
+const { aws4Interceptor } = require('aws4-axios');
+const HttpClient = require('@artistdirectory/http-client').default;
+
+const { AWS_REGION, DIRECTORY_API_URL } = process.env;
+
+const httpClient = new HttpClient({
+  baseUrl: DIRECTORY_API_URL
+});
+
+httpClient.addRequestInterceptor(
+  aws4Interceptor({
+    region: AWS_REGION,
+    service: 'execute-api'
+  })
+);
+
+const handler = middy(async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  if (event.source === 'serverless-plugin-warmup') {
+    await new Promise((resolve) => setTimeout(resolve, 25));
+    return 'Lambda is warm!';
+  }
+
+  try {
+    const tags = await httpClient.get('/tags');
+
+    return {
+      statusCode: StatusCodes.OK,
+      body: JSON.stringify(tags)
+    };
+  } catch (error) {
+    if (error.response && error.response.status) {
+      return {
+        statusCode: error.response.status,
+        body:
+          JSON.stringify(error.response.data) ||
+          getReasonPhrase(error.response.status)
+      };
+    }
+
+    return {
+      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      body: error.message || ReasonPhrases.INTERNAL_SERVER_ERROR
+    };
+  }
+});
+
+handler.use(cors());
+
+module.exports.handler = handler;
