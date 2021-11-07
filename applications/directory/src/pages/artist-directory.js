c0533860d16e6418ed3daf3f631930e775954509
Chad Johnson <chad.d.johnson@gmail.com>
2021-11-06 23:05:16 -0400

    Added artist directory page skeleton.

---
+++ applications/directory/src/pages/artist-directory.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import { Layout } from '../components';
+
+const ArtistDirectoryPage = () => (
+  <>
+    <Head>
+      <title>Artist Directory</title>
+    </Head>
+    <Layout>List here</Layout>
+  </>
+);
+
+export default ArtistDirectoryPage;
