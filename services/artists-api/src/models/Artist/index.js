b8e4402e60ade9e954d84b685b6063ac07d52168
Chad Johnson <chad.d.johnson@gmail.com>
2021-11-06 21:23:47 -0400

    Fleshed out Artist model.

--- services/artists-api/src/models/Artist/index.js
+++ services/artists-api/src/models/Artist/index.js
@@ -8,7 +8,32 @@ const schemaOptions = {
 };
 const schema = new mongoose.Schema(
   {
-    name: { type: String, required: true }
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    city: { type: String, required: true },
+    social: { type: mongoose.Schema.Types.Mixed, required: true },
+    artistType: { type: mongoose.Schema.Types.Mixed, required: true },
+    description: { type: String, required: true },
+    keywords: {
+      type: [String],
+      validate: {
+        validation: (value) => {
+          return !value?.length > 0;
+        }
+      },
+      required: true
+    },
+    hireableSkills: {
+      type: [String],
+      validate: {
+        validation: (value) => {
+          return !value?.length > 0;
+        }
+      },
+      required: true
+    },
+    subscribedToNewsletter: { type: Boolean, required: true }
   },
   schemaOptions
 );
