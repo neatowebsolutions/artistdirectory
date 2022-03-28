56bba51d52715532999400d3cf74056c1fe9cce4
OxiBo <xenos2000@ukr.net>
2022-03-27 21:52:15 -0400

    Add Formik to CreateProfileForm component, form validation with Yup

--- applications/directory/src/pages/profile/create.js
+++ applications/directory/src/pages/profile/create.js
@@ -1,90 +1,117 @@
-import Head from 'next/head';
-import Box from '@mui/material/Box';
-import { Layout } from '../../components';
-import CreateProfileForm from '../../components/CreateProfileForm';
+import Head from "next/head";
+import Box from "@mui/material/Box";
+import { Layout } from "../../components";
+import CreateProfileForm from "../../components/CreateProfileForm";
+import { useCategories, useTags, useSkills } from "../../hooks";
 
-const CreatePage = () => (
-  <>
-    <Head>
-      <title>Create Profile</title>
-    </Head>
-    <Layout>
-      <Layout.Intro>
-        <Box
-          sx={{
-            '& h1': {
-              typography: 'body2',
-              fontSize: '3.75rem',
-              fontWeight: 'bold',
-              letterSpacing: '-0.5px',
-              mt: '1.5rem'
-            }
-          }}
-        >
+const CreatePage = () => {
+  const { categories, categoriesLoading, categoriesError } = useCategories();
+  const { skills, skillsLoading, skillsError } = useSkills();
+  const { tags, tagsLoading, tagsError } = useTags();
+
+  return (
+    <>
+      <Head>
+        <title>Create Profile</title>
+      </Head>
+      <Layout>
+        <Layout.Intro>
           <Box
             sx={{
-              '& p': {
-                typography: 'body1',
-                fontWeight: 'bold',
-                fontSize: '1.5rem',
-                textTransform: 'uppercase',
-                letterSpacing: '3.53px',
-                mb: '1.5rem'
-              }
-            }}
-          >
-            <p>Create your artist profile</p>
-          </Box>
-
-          <h1>Local Artist Network</h1>
-          <Box sx={{ maxWidth: 1080, margin: '0 auto' }}>
-            <p>
-              By submitting this form, you agree to have the following listed in
-              the LAN: name, profession, website, Instagram, a thumbnail image
-              of work, and a short description of your talents and services.
-            </p>
-          </Box>
-          <Box sx={{ maxWidth: 814, margin: '0 auto' }}>
-            <p>
-              We&apos;re building a directory of the artists (art, design,
-              music, performance) in the greater Grand Rapids area, so that
-              people can discover their work all in once place. Whether
-              you&apos;re looking to sell work, get hired for a project, or
-              collaborate with another artist, the Local Artist Network can help
-              you reach your goal.
-            </p>
-          </Box>
-          <Box
-            sx={{
-              '& p span': {
-                fontStyle: 'italic'
+              "& h1": {
+                typography: "body2",
+                fontSize: "3.75rem",
+                fontWeight: "bold",
+                letterSpacing: "-0.5px",
+                mt: "1.5rem",
               },
-              '& p a': {
-                textTransform: 'uppercase',
-                fontWeight: 'bold',
-                letterSpacing: '1.25px',
-                color: 'primary.main'
-              }
             }}
           >
-            <p>
-              <span>
-                You may remove yourself from this listing at any time by
-                emailing
-              </span>
-              <br />
-              <a href="mailto:avenueforthearts@gmail.com">
-                avenueforthearts@gmail.com
-              </a>
-            </p>
+            <Box
+              sx={{
+                "& p": {
+                  typography: "body1",
+                  fontWeight: "900",
+                  fontSize: "24px",
+                  textTransform: "uppercase",
+                  letterSpacing: "3.53px",
+                  mb: "1.5rem",
+                  lineHeight: "1.5",
+                },
+              }}
+            >
+              <p>Grand Rapids Artist Directory</p>
+            </Box>
+            <Box
+              sx={{
+                color: "primary.main",
+                letterSpacing: 10,
+                "& span": {
+                  fontSize: "60px",
+                  fontWeight: "bold",
+                  lineHeight: "72px",
+                  letterSpacing: "3px",
+                  textTransform: "uppercase",
+                  color: "primary.main",
+                },
+              }}
+            >
+              <span>Get found.</span>
+            </Box>
+            <Box sx={{ maxWidth: 1080, margin: "0 auto" }}>
+              <p>
+                By submitting this form you agree to have the following listed
+                in the LAN: name, profession, website, Instagram, a thumbnail
+                image of work, and a short description of your talents and
+                services.
+              </p>
+            </Box>
+            <Box sx={{ maxWidth: 814, margin: "0 auto" }}>
+              <p>
+                We’re building a directory of the artists (art, design, music,
+                performance) in the greater Grand Rapids area, so that people
+                can discover their work all in once place. Whether you’re
+                looking to sell work, get hired for a project, or collaborate
+                with another artist, the Local Artist Network can help you reach
+                your goal.
+              </p>
+            </Box>
+            <Box
+              sx={{
+                "& p span": {
+                  fontStyle: "italic",
+                },
+                "& p a": {
+                  textTransform: "uppercase",
+                  fontWeight: "bold",
+                  letterSpacing: "1.25px",
+                  color: "primary.main",
+                },
+              }}
+            >
+              <p>
+                <span>
+                  You may remove yourself from this listing at any time by
+                  emailing
+                </span>
+                <br />
+                <a href="mailto:avenueforthearts@gmail.com">
+                  avenueforthearts@gmail.com
+                </a>
+              </p>
+            </Box>
           </Box>
+        </Layout.Intro>
+        <Box sx={{ maxWidth: "782px", margin: "0 auto" }}>
+          <CreateProfileForm
+            skills={skills}
+            tags={tags}
+            categories={categories}
+          />
         </Box>
-      </Layout.Intro>
-      <Box sx={{ maxWidth: '782px', margin: '0 auto' }}>
-        <CreateProfileForm />
-      </Box>
-    </Layout>
-  </>
-);
+      </Layout>
+    </>
+  );
+};
 
 export default CreatePage;
