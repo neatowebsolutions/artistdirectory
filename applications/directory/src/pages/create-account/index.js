b0d8fd1f8f7e30deceeeaf79e7140a0b3c5d50ff
OxiBo <xenos2000@ukr.net>
2022-08-25 19:51:21 -0400

    Revert "Removed create-account page."
    This reverts commit af1f5b2ff690d3b8790fa09188982af7873b0136.

---
+++ applications/directory/src/pages/create-account/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import Box from '@mui/material/Box';
+import { Layout } from '../../components';
+import ThankYou from '../../components/ThankYou';
+import CreateAccount from '../../components/CreateAccount';
+
+function CreateAccountPage() {
+  return (
+    <>
+      <Head>
+        <title>Create Your Account</title>
+      </Head>
+      <Layout>
+        <Layout.Intro>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: ['column', 'row'],
+              alignItems: ['start']
+            }}
+          >
+            <ThankYou />
+            <CreateAccount />
+          </Box>
+        </Layout.Intro>
+      </Layout>
+    </>
+  );
+}
+
+export default CreateAccountPage;
