94912dcd6425b147cf4f44e682f4af15e25929c1
OxiBo <xenos2000@ukr.net>
2022-09-07 11:23:49 -0400

    Add and clean up styles in thank-you page (profile creation)

--- applications/directory/src/pages/profile/thank-you.js
+++ applications/directory/src/pages/profile/thank-you.js
@@ -1,7 +1,6 @@
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Box from '@mui/material/Box';
-
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import { Layout } from '../../components';
@@ -25,64 +24,68 @@ const ThankYouPage = () => {
               sx={{
                 textAlign: 'center',
                 width: ['100%', '80%', '70%'],
-                margin: '0 auto',
-                '& h2': {
+                margin: '0 auto'
+              }}
+              elevation={6}
+            >
+              <Typography
+                variant="h2"
+                component="h2"
+                sx={{
                   fontWeight: '900',
                   fontSize: ['0.875rem', '1.25rem', '1.5rem'],
                   textTransform: 'uppercase',
                   letterSpacing: 3.53,
                   marginBottom: ['1.063rem', '1.625rem', '1.5rem']
-                }
-              }}
-              elevation={6}
-            >
-              <Typography variant="h2" component="h2">
+                }}
+              >
                 Grand Rapids Artist Directory
               </Typography>
-              <Box
-                sx={{
-                  '& h1': {
+              <Box>
+                <Typography
+                  variant="h2"
+                  component="h2"
+                  sx={{
                     fontFamily: 'gira-sans, sans-serif',
                     lineHeight: ['normal', '1.17', '1.2'],
                     textTransform: 'uppercase',
                     color: 'success.main',
                     letterSpacing: [2, 3]
-                  }
-                }}
-              >
-                <Typography variant="h1" component="h1">
+                  }}
+                >
                   Get found.
                 </Typography>
               </Box>
-              <Box
-                sx={{
-                  '& h3': {
-                    color: 'secondary.secondary',
-                    fontWeight: 'bold',
-                    margin: 'margin: 1.375rem 0 0.813rem'
-                  },
-                  '& p': {
-                    color: 'secondary.secondary',
-                    fontWeight: '500',
-                    margin: '0.813rem 0 0'
-                  }
-                }}
-              >
+              <Box>
                 <Box
                   component="img"
                   src="/images/img-success.svg"
                   alt="Success"
                   sx={{
-                    width: ['6rem', '9rem'],
-
-                    marginTop: ['1.5rem', '2rem'],
+                    width: ['4rem', '5rem', '9rem'],
+                    marginTop: ['1rem', '1.5rem', '2.6rem'],
                     marginBottom: ['1rem', '1.3rem']
                   }}
                 />
-                <Typography variant="h3" component="h3">
+                <Typography
+                  variant="h3"
+                  component="h3"
+                  sx={{
+                    color: 'secondary.secondary',
+                    fontWeight: 'bold',
+                    margin: 'margin: 1.375rem 0 0.813rem'
+                  }}
+                >
                   Thanks, {query.name}!
                 </Typography>
-                <Typography variant="body1">
+                <Typography
+                  variant="body1"
+                  sx={{
+                    color: 'secondary.secondary',
+                    fontWeight: '500',
+                    margin: '0.813rem 0 0'
+                  }}
+                >
                   Your profile has been submitted for review!
                 </Typography>
               </Box>
