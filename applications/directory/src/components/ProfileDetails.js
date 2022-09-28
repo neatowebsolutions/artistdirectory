b8721a3465acad534711f09ec0d4dfbf30599fbe
OxiBo <xenos2000@ukr.net>
2022-09-28 17:05:56 -0400

    Svg icons styling in CreateProfileForm and ProfileDetails components

--- applications/directory/src/components/ProfileDetails.js
+++ applications/directory/src/components/ProfileDetails.js
@@ -1,6 +1,5 @@
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
-import Icon from '@mui/material/Icon';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import EditIcon from '@mui/icons-material/Edit';
@@ -17,22 +16,13 @@ const displayUrlReg = /http(s)?(:)?(\/\/)?|(\/\/)?(www\.)?/;
 const socialIcons = {
   website: <LanguageIcon />,
   behance: (
-    <SvgIcon component={BehanceIcon} sx={{ backgroundColor: 'orange' }} />
-  ), // TODO - it does not show the icon
-  // behance: (
-  //   <Icon sx={{ '& img': { marginBottom: '0.4rem', marginRight: '2rem' } }}>
-  //     <img src="/images/img-behance.svg" alt="" />
-  //   </Icon>
-  // ),
-  // behance: (
-  //   <SvgIcon>
-  //     {' '}
-  //     <path
-  //       d="M18.3331354,1.66664867 L12.499865,1.66664867 L12.499865,0 L18.3331354,0 L18.3331354,1.66664867 Z M19.7714532,9.99989201 C19.4031238,11.0807137 18.0806381,12.499865 15.5206657,12.499865 C12.9590267,12.499865 10.8840491,11.0590472 10.8840491,7.77074942 C10.8840491,4.51245127 12.8215282,2.83746936 15.4389999,2.83746936 C18.0073055,2.83746936 19.5756219,4.32245332 19.9181182,6.52576286 C19.9831175,6.94742497 20.0089506,7.51575217 19.997284,8.30907694 L13.3081896,8.30907694 C13.4165218,10.9848814 16.2106583,11.0690471 17.1314817,9.99989201 L19.7714532,9.99989201 Z M13.3665223,6.66659467 L17.5039776,6.66659467 C17.4164786,5.37744193 16.5573212,4.81744798 15.4398333,4.81744798 C14.2181798,4.81744798 13.5423538,5.45744106 13.3665223,6.66659467 Z M5.38827515,12.4898651 L-7.4014069e-16,12.4898651 L-7.4014069e-16,0.017499811 L5.7941041,0.017499811 C10.3573881,0.0849990821 10.4440539,4.55411749 8.06074628,5.77243766 C10.9448818,6.82242632 11.0415474,12.4898651 5.38827515,12.4898651 Z M2.499973,4.99994601 L5.48660742,4.99994601 C7.57658485,4.99994601 7.90824793,2.499973 5.22661022,2.499973 L2.499973,2.499973 L2.499973,4.99994601 Z M5.32577582,7.49991901 L2.499973,7.49991901 L2.499973,10.0132252 L5.2841096,10.0132252 C7.82991544,10.0132252 7.67408379,7.49991901 5.32577582,7.49991901 L5.32577582,7.49991901 Z"
-  //       id="Shape"
-  //     ></path>
-  //   </SvgIcon>
-  // ),
+    <SvgIcon
+      component={BehanceIcon}
+      sx={{
+        transform: 'translate(17%,15%)'
+      }}
+    />
+  ),
   other: <AltRouteIcon />
 };
 
@@ -85,7 +75,6 @@ const ProfileDetails = ({ artist, isLoggedIn = false }) => {
         }}
       >
         <img src={profileImageUrl} alt="Profile" />
-        {/* <BehanceIcon /> */}
       </Box>
       <Box
         sx={{
@@ -176,35 +165,31 @@ const ProfileDetails = ({ artist, isLoggedIn = false }) => {
 
         {social.length > 0 &&
           social.map(({ name, url }, index) => (
-            <Box
-              key={index}
-              sx={{ marginTop: '1rem', backgroundColor: 'yellow' }}
-            >
+            <Box key={index} sx={{ marginTop: '1.25rem' }}>
               <Box
                 sx={{
                   display: 'flex',
                   alignItems: 'center',
-                  justifyContent: ['center', 'flex-start'],
-                  backgroundColor: 'green'
+                  justifyContent: ['center', 'flex-start']
                 }}
               >
                 <Box
                   component="span"
                   sx={{
+                    display: 'flex',
+                    alignItems: 'center',
                     '& svg': {
-                      fontSize: 20,
+                      fontSize: 24,
                       fontWeight: 'bold',
                       marginRight: '5px',
                       color: 'primary.main',
-                      fill: 'purple',
-                      '& path': {
-                        fill: 'primary.main'
-                      }
+                      margin: '0.125rem 0.5rem 0.125rem 0'
                     }
                   }}
                 >
                   {socialIcons[name]}
                 </Box>
+
                 <Box component="span" sx={labelStyles}>
                   {name}
                 </Box>
